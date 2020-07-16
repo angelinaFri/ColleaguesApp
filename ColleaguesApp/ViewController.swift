ba8f2a00f85145b12ef19a515895f4397d0d0f23
Angelina F <angelina.friz@gmail.com>
2020-07-16 21:53:46 +0300

    Initial Commit

---
+++ ColleaguesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColleaguesApp
+//
+//  Created by Angelina on 16.07.2020.
+//  Copyright © 2020 Angelina Friz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
